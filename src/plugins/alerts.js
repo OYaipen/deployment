f82c13e87ec49110ac09d3d1e3771001c6301c6b
Owen Yaipen <1999yaipen@gmail.com>
2019-12-16 23:15:40 -0500

    translate alert

--- src/plugins/alerts.js
+++ src/plugins/alerts.js
@@ -3,11 +3,10 @@ import i18n from '~/plugins/i18n'
 
 window.addEventListener('offline', function () {
     Swal.fire({
-        icon: 'error',
+        icon: 'warning',
         title: i18n.t('error_alert_title'),
-        text: i18n.t('error_alert_text'),
+        text: i18n.t('error_conection'),
         reverseButtons: true,
         confirmButtonText: i18n.t('ok'),
-        cancelButtonText: i18n.t('cancel')
     })
 });
